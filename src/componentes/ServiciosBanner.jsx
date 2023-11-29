90081a0669368bdbdc51417c4f30c2b08404cf31
Boris P <boris.p.espinoza@gmail.com>
2023-11-29 16:41:30 -0300

    Proyecto-5elementos

---
+++ src/componentes/ServiciosBanner.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+
+export const ServiciosBanner = (image, title, text) => {
+    return (
+        <div className="jumbotron">
+            <img className="img-fluid" src="a.png" alt="imagen logo"/>
+        </div>
+    )
+
+}
